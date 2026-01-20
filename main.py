00aa6500e3ac29f92794ac8a24c0e07fcf6b82c9
Alec Ghost <alexander.ghost@web.de>
2026-01-20 11:36:58 +0100

    Call benchmark in main

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import ollama
-from typing import Callable
-from rich.logging import RichHandler
 import logging
-from enum import Enum
 import sys
-from sys import exit
-from agentdojo.functions_runtime import FunctionsRuntime
-from openai import OpenAI
-from agentdojo.agent_pipeline import (
-    AgentPipeline,
-    SystemMessage,
-    InitQuery,
-)
-from agentdojo.logging import OutputLogger
+from enum import Enum
+from pathlib import Path
+
+import agentdojo.agent_pipeline as pipeline
+import agentdojo.benchmark as bench
+import agentdojo.task_suite as task_suite
 import openai
+from agentdojo.logging import OutputLogger
+from rich.logging import RichHandler
+
+import lamb.tool_execution as tools
 from lamb.prompting_llm import PromptingLLM
-from lamb.tool_execution import ToolsExecutionLoop, ToolsExecutor
 
 
 class OllamaModel(Enum):
@@ -27,48 +23,55 @@ class OllamaModel(Enum):
     MISTRAL = "mistral:7b"
     PHI = "phi3:3.8b"
 
+
 MODEL = OllamaModel.LLAMA3
 
 
 def local_llm(model: OllamaModel) -> PromptingLLM:
     try:
-        client = OpenAI(
+        client = openai.OpenAI(
             base_url="http://localhost:11434/v1",
             api_key="ollama",  # required, but unused
         )
         return PromptingLLM(client, model.value)
     except openai.APIConnectionError:
         print("Ollama must be running", file=sys.stderr)
-        exit(1)
+        sys.exit(1)
 
 
 def main():
     llm = local_llm(MODEL)
-    tools_loop = ToolsExecutionLoop(
+    tools_loop = tools.ToolsExecutionLoop(
         [
-            ToolsExecutor(),
+            tools.ToolsExecutor(),
             llm,
         ]
     )
-    tools_pipeline = AgentPipeline(
+    tools_pipeline = pipeline.AgentPipeline(
         [
-            SystemMessage(
+            pipeline.SystemMessage(
                 "You are a helpful AI assistant with tool-calling capabilities."
             ),
-            InitQuery(),
+            pipeline.InitQuery(),
             llm,
             tools_loop,
         ]
     )
+    # This is an inconsistency in AgentDojo.
+    # The benchmark requires the attribute `name` to be set,
+    # but the `AgentPipeline` class doesn't have it.
+    tools_pipeline.__setattr__("name", "lamb")
 
-    runtime = FunctionsRuntime([])
-    for f in functions():
-        runtime.register_function(f)
-
+    slack_suite = task_suite.get_suite("v1.2", "slack")
     with OutputLogger(None, None):
-        tools_pipeline.query(
-            input("> "), runtime
+        results = bench.benchmark_suite_without_injections(
+            suite=slack_suite,
+            agent_pipeline=tools_pipeline,
+            logdir=Path("./logs"),
+            force_rerun=False,
         )
+        utility = bench.aggregate_results([results["utility_results"]])
+        print(f"Utility: {utility}")
 
 
 def configure_logging():
@@ -79,29 +82,6 @@ def configure_logging():
         datefmt="%H:%M:%S",
         handlers=[RichHandler(show_path=False, markup=True)],
     )
-
-
-def functions() -> list[Callable]:
-    def sum(a: int, b: int):
-        """Sums two numbers.
-
-        :param a: the first number
-        :param b: the second number
-        """
-        return a + b
-
-    def web_search(query: str):
-        """Search the web. You MUST use this function for real-time information.
-
-        :param query: the query for the search bar
-        """
-        return ollama.generate(
-            model=MODEL.value,
-            system="Give a straight-forward answer to the provided questions",
-            prompt=query,
-        ).response
-
-    return [sum, web_search]
 
 
 if __name__ == "__main__":
