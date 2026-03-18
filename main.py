65ea85b102ab650f59da9bbf7cb3fba0cf06de92
Alec Ghost <alexander.ghost@web.de>
2026-03-18 18:29:02 +0100

    Add main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from lamb import (
     agent,
     bench,
+    labels,
     llm,
 )
 from lamb.agent import Agent
@@ -13,15 +14,20 @@ def main() -> None:
     model = llm.LiveLlm.ollama_openai(llm.OllamaModel.GPT_OSS_120B)
     bench.benchmark(
         agent_loop=agent.ADAgentLoop(
-            lambda runtime, env: Agent.lamb_no_ifc(model, runtime, env)
+            lambda runtime, env: Agent.lamb_dynamic_ifc(
+                model,
+                runtime,
+                env,
+                labels.ADLabeler(env),
+            )
         ),
         model=model.model,
-        agent="lamb-no-ifc",
+        agent="local-lamb-dynamic-ifc",
         log_dir=log_dir,
-        attack=None,
-        suites=["slack"],
-        user_tasks=[],
-        injection_tasks=["injection_task_0", "injection_task_1"],
+        attack="tool_knowledge",
+        suites=None,
+        user_tasks=None,
+        injection_tasks=None,
         suite_version="v1.2",
         n_repeats=1,
         force_rerun=False,
